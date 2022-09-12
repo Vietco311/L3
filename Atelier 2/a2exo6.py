cc53bf06f81d453a250182469adced988fdc705a
Vietco311 <ant.colin311@gmail.com>
2022-09-12 13:57:21 +0200

    Création des dossiers correspondants

--- Atelier 2/a2exo6.py
+++ Atelier 2/a2exo6.py
@@ -24,17 +24,19 @@ def present2 (L, e) :
 
 def present3 (L, e) :
     b=True
-    for i in range (0, len(L), 1):
-        return (L[i] == e)
+    i=0
+    while L[i] != e:
+        return (i == e)
+    
 
 def present4 (L, e) :
     b=False
     i=0
-    while (i<len(L) and b) :
+    while (i<len(L)) :
         if (L[i] == e) :
             b=True
-            return (b)
-
+        i += 1
+    return b
 
 def test_present(present:callable):
     LISTE = [0, 3, 17, 4, 8, 0, 6, -4, -8, 7, 11, 0, 14, 0, -19]
